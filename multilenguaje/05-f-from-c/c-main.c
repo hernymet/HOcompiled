2cf6ef2fb68afe44a2e2fc79daa448274addd3d3
hernymet <hernymet@github>
2015-11-05 15:46:18 -0300

    completa la lamada de c a fortran

--- multilenguaje/05-f-from-c/c-main.c
+++ multilenguaje/05-f-from-c/c-main.c
@@ -1,18 +1,19 @@
 
 #include <stdio.h>
-#include "c-sum.h"
+//#include "c-sum.h"//
 
 #define NUM 200
 
+extern void sub_abs_(int *data, int *num, int *out);
+
 int main(int argc, char **argv)
 {
-    int data[NUM], num, i;
-
+    int data[NUM], num, i, out;
     num = NUM;
     for (i=0; i < num; i++) {
         data[i] = i - 100 + 1;  /* NOTE: difference in array numbering */  
     }
-
-    printf("sum=%d\n", sum_abs(data, num));
+    sum_abs_(data, &num, &out);
+    printf("sum=%d\n", out);
     return 0;
 }
